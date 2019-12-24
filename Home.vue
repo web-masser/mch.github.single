f30a665892846372ff9e93c3af7d056bd4906484
孟成华 <916521009@qq.com>
2019-12-24 16:18:05 +0800

    初始化

---
+++ Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        
+    </div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from "vue-property-decorator";
+import { Action, Getter } from "vuex-class";
+
+@Component
+export default class cluesReport extends Vue {
+   
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
